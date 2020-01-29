69562a20e76cde33068899f47070a1a6d9b90da2
Vinicius Martins <vini.martinslol@gmail.com>
2020-01-29 13:51:12 -0300

    Cria loader model

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/Users';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Vinicius Martins',
+    email: 'vini.martinslol@gmail.com',
+    password_hash: '515444664',
+  });
+  return res.json(user);
 });
 
 export default routes;
